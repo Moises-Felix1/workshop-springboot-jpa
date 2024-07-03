5f25fe126d8e5ea83c4ef49c1a2d0aaea16805f1
Moises Felix <moises012felix@gmail.com>
2024-07-03 11:39:34 -0300

    Category

--- src/main/java/com/deb/course_spring/config/TestConfig.java
+++ src/main/java/com/deb/course_spring/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.deb.course_spring.entities.Category;
 import com.deb.course_spring.entities.Order;
 import com.deb.course_spring.entities.User;
 import com.deb.course_spring.entities.enums.OrderStatus;
+import com.deb.course_spring.repositories.CategoryRepository;
 import com.deb.course_spring.repositories.OrderRepository;
 import com.deb.course_spring.repositories.UserRepository;
 
@@ -18,14 +20,25 @@ import com.deb.course_spring.repositories.UserRepository;
 @Profile("test")
 public class TestConfig implements CommandLineRunner{
 
+	// Injecao de dependencia
 	@Autowired
 	private UserRepository userRepository;
 	
+	// Injecao de dependencia
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	// Injecao de dependencia
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
@@ -33,6 +46,7 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2); 
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 	
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}
--- src/main/java/com/deb/course_spring/entities/Order.java
+++ src/main/java/com/deb/course_spring/entities/Order.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 
 import com.deb.course_spring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
